1f235252b77af702cf1320d54a1d8ecffa7d325f
Elroy Chua <83333253+elroychua@users.noreply.github.com>
2021-12-03 17:10:26 +0800

    Release UOWRoombooking

---
+++ UOWRoombooking/UOWRoombooking/UOWRoombookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  UOWRoombookingApp.swift
+//  UOWRoombooking
+//
+//  Created by Elroy C. on 11/10/21.
+//
+
+import SwiftUI
+
+@main
+struct UOWRoombookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
